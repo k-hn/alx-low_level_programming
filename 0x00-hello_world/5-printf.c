8374cda7c3b417aa2e3eb9833c16a09ca535bce6
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-16 12:09:03 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with the puts function
+ *
+ * Return: 0 - Always success
+ */
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
