ebd77595ed7824c0635c62ec95c07860645c342e
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-17 09:49:29 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints that prints
+ * all the lowercase alphabets except the alphates e and q.
+ *
+ * Return: 0 - Always success
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		printf("%d", count);
+	}
+	putchar('\n');
+
+	return (0);
+}
