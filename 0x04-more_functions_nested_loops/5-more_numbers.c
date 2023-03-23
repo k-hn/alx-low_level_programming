4a17484b8f09693c9ff7f8390a4a3650c438356f
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-23 05:11:37 -0700

    refactor 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,33 +8,37 @@
  */
 void more_numbers(void)
 {
+	int count;
 	int outer_count;
 	int inner_count;
 
-	for (outer_count = 0; outer_count <= 1; outer_count++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (inner_count = 0; inner_count <= 14; inner_count++)
+		for (outer_count = 0; outer_count <= 1; outer_count++)
 		{
-			/* This break is necessary to prevent printing of 0 - 4 after 9 */
-			/* also prevents printing a 0 after 9 */
-			if (outer_count == 0 && inner_count > 9)
+			for (inner_count = 0; inner_count <= 14; inner_count++)
 			{
-				break;
-			}
+				/* This break is necessary to prevent printing of 0 - 4 after 9 */
+				/* also prevents printing a 0 after 9 */
+				if (outer_count == 0 && inner_count > 9)
+				{
+					break;
+				}
 
-			/* stop sequence at 14 */
-			if (outer_count == 1 && inner_count > 4)
-			{
-				break;
-			}
+				/* stop sequence at 14 */
+				if (outer_count == 1 && inner_count > 4)
+				{
+					break;
+				}
 
-			if (outer_count != 0)
-			{
-				_putchar('0' + outer_count);
-			}
+				if (outer_count != 0)
+				{
+					_putchar('0' + outer_count);
+				}
 
-			_putchar('0' + (inner_count % 10));
+				_putchar('0' + (inner_count % 10));
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
