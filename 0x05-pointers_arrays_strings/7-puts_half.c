bcfa5314ee33cbbbc732734cc75d3838fdd14c4c
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-28 09:30:40 -0700

    refactor 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,23 @@
 void puts_half(char *str)
 {
 	int str_length = strlen(str);
-	int print_threshold = str_length / 2;
+
 	int count;
 
-	for (count = print_threshold; count < str_length; count++)
+	if (str_length % 2 == 0)
 	{
-		_putchar(str[count]);
+		for (count = str_length / 2; count < str_length; count++)
+		{
+			_putchar(str[count]);
+		}
+	}
+	else
+	{
+		for (count = (str_length / 2) + 1; count < str_length; count++)
+		{
+			_putchar(str[count]);
+		}
 	}
 	_putchar('\n');
+
 }
