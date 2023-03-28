b2d0638f392d9b40de8d30c1172a172598d26b57
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-28 09:01:16 -0700

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for(count = 0; str[count] != '\0'; count++)
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(str[count]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
