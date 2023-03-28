4c0aec2c704448a9bd635d517caefe0209b304df
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-28 10:01:44 -0700

    refactor 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,14 @@ void print_array(int *a, int n)
 
 	for (count = 0; count < n; count++)
 	{
-		printf("%d", a[count]);
-
 		if (count == n - 1)
 		{
-			printf("\n");
+			printf("%d", a[count]);
 		}
 		else
 		{
-			printf(", ");
+			printf("%d, ", a[count]);
 		}
 	}
+	printf("\n");
 }
