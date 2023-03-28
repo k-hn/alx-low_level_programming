0f7c69422a14e1bc7e3fe2c7824aeb9bd295ccdd
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-28 07:54:53 -0700

    add 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,9 @@
 void print_rev(char *s)
 {
 	int count;
-	/*int s_length = sizeof(s) / sizeof(*s);*/
 	int s_length = strlen(s);
 
-	for (count = s_length; count >= 0; count--)
+	for (count = s_length - 1; count >= 0; count--)
 	{
 		_putchar(s[count]);
 	}
