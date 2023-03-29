58d9a9217e98a2a5fb965e81fcd60f0e868cfc40
Hilary <125813796+k-hn@users.noreply.github.com>
2023-03-29 11:29:59 -0700

    refactor 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,22 @@
 char *cap_string(char *s)
 {
 	int count;
+	int i;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (count = 0; s[count] != '\0'; count++)
 	{
-		if (s[count - 1] == ' ' && s[count] >= 'a' && s[count] <= 'z')
+		for (i = 0; i < 13; i++)
 		{
-			s[count] = s[count] - 32;
-		}
-		else
-		{
-			continue;
+			if (s[count - 1] == spe[i] && s[count] >= 'a' && s[count] <= 'z')
+			{
+				s[count] = s[count] - 32;
+			}
+			else
+			{
+				continue;
+			}
 		}
 	}
 
