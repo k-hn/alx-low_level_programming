023426e40ce6eb39dd9d85a92d8bc6f8b797be13
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-06 09:56:38 -0700

    add 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument array variable
+ *
+ * Return: 0 - always successful
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+
+	return (0);
+}
