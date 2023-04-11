a9a01bd8f6705543d5ac6b16d1b1b2a53fc885db
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-11 10:42:46 -0700

    refactor 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,11 @@ char *create_array(unsigned int size, char c)
 	}
 
 	ar = malloc(sizeof(char) * size);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
 	for (count = 0; count < size; count++)
 	{
 		ar[count] = c;
