210bced8413f8b2eba173d3ce616fc7198ca0db3
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-11 05:16:53 -0700

    refactor 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,14 @@
-int _atoi(char *s);
+#include "holberton.h"
+
+/**
+ * atoi - converts a string into an integer
+ *
+ * @s: string input pointer
+ *
+ * Return: 0 if there is no number
+*/
+
+int _atoi(char *s)
+{
+
+}
