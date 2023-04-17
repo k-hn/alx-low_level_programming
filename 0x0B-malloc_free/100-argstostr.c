c4b1655eaf71fd4eebb20c317b63a767b439c596
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-17 10:40:49 -0700

    add 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all args of program
+ *
+ * @ac: arg counter
+ * @av: arg array
+ *
+ * Return: concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int count, i, j;
+	char *accum;
+	int required_size = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 1; count < ac; count++)
+	{
+		required_size += strlen(av[count]);
+	}
+
+	/* malloc required_size */
+	accum = malloc(sizeof(char) * (required_size + (ac - 1)));
+	if (accum == NULL)
+	{
+		return (NULL);
+	}
+
+	/* begin concat */
+	count = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			accum[count] = av[i][j];
+			count++;
+		}
+
+		accum[count] = '\n';
+		count++;
+	}
+
+	return (accum);
+}
