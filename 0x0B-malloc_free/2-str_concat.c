635e5379bab93dc33a77f3f8db3a1836e6ca4abf
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-17 06:21:26 -0700

    refactor 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,44 +15,25 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *accum;
-	int count;
-	int s1_length;
-	int s2_length;
+	int count, s1_length, s2_length;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-
 	s1_length = strlen(s1);
 	s2_length = strlen(s2);
-	
 	accum = malloc(sizeof(char) * (s1_length + s2_length + 1));
 	if (accum == NULL)
-	{
 		return (NULL);
-	}
-
-	
-       
 	for (count = 0; s1[count] != '\0'; count++)
 	{
 		accum[count] = s1[count];
 	}
-	
-
 	for (count = 0; s2[count] != '\0'; count++)
 	{
-	        accum[s1_length + count] = s2[count];
+		accum[s1_length + count] = s2[count];
 	}
-      
-
 	accum[s1_length + s2_length] = '\0';
-
 	return (accum);
 }
