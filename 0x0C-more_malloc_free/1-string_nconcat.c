95eb80a46c5c9782b8993bab1627d5afc784d9fc
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-18 09:47:00 -0700

    refactor 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,22 +20,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	s1_length = strlen(s1);
 	s2_length = strlen(s2);
+
 	if (n >= s2_length)
 		n = s2_length;
-	accum = malloc(sizeof(*accum) * (s1_length + s2_length + 1));
+
+	accum = malloc(sizeof(char) * (s1_length + n + 1));
 	if (accum == NULL)
 		return (NULL);
+
 	for (s1_count = 0; s1[s1_count] != '\0'; s1_count++)
 	{
 		accum[s1_count] = s1[s1_count];
 	}
+
 	for (s2_count = 0; s2[s2_count] != '\0' && s2_count < n; s2_count++)
 	{
 		accum[s1_count] = s2[s2_count];
 		s1_count++;
 	}
+
 	accum[s1_count] = '\0';
 	return (accum);
 }
