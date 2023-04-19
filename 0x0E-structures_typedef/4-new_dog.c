56594d76d1ef75d87f262cc0248896dd082baa67
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-19 03:28:02 -0700

    refactor 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (owner_length + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
