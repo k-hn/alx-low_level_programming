5a5d6a477edf51894fef8fdba044d17e03e6ada4
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-19 08:07:36 -0700

    add 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 - successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	
+}
