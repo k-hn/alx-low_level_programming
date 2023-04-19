36cf66933f5faa7bf9bfc5bbee5a195caf1f933a
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-19 15:24:14 -0700

    fix formatting

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,23 +10,23 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int count = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int count = 0;
 
-    while (count < 5)
-      {
-	if (strcmp(s, ops[count].op) == 0)
-	  {
-	    return (ops[count].f);
-	  }
-	count++;
-      }
-    return (NULL);
+	while (count < 5)
+	{
+		if (strcmp(s, ops[count].op) == 0)
+		{
+			return (ops[count].f);
+		}
+		count++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,10 +13,10 @@
  */
 int main(int argc, char *argv[])
 {
-        int a;
+	int a;
 	int b;
 	int (*operator)(int, int);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -26,13 +26,13 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	operator = get_op_func(argv[2]);
-	
+
 
 	if (operator == NULL)
-	  {
-	    printf("Error\n");
-	    exit(99);
-	  }
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	printf("%d\n", operator(a, b));
 
