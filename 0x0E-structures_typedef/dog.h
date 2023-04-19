759e25f2bddb1b608ff2bd007530b6b3574ac003
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-18 18:25:21 -0700

    refactor dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
+/**
+ * struct dog - dog structure
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of dog owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
