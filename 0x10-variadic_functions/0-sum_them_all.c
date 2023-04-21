296db8f6411ae48b94a66371871afedb0171a6af
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-21 16:47:34 +0000

    refactor 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int count;
 	int sum = 0;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+	
 	va_start(args_pointer, n);
 
 	for (count = 0; count < n; count++)
