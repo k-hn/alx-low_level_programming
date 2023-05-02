018b0310142f8a56a04526e027887aa57d5902ee
Hilary <125813796+k-hn@users.noreply.github.com>
2023-05-02 16:25:03 +0000

    refactor 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while(count < index - 1)
+	while (count < index - 1)
 	{
 		if (!temp_node || !(temp_node->next))
 		{
