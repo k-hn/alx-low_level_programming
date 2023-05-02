9a5a3c5514a07302ea6d2f156142232b67cdeee6
Hilary <125813796+k-hn@users.noreply.github.com>
2023-05-02 16:10:39 +0000

    add 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,43 +12,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *current_node;
 	unsigned int count;
+	listint_t *new_node;
+	listint_t *temp_node = *head;
 
-	current_node = *head;
 	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
+	if (!new_node || !head)
 	{
 		return (NULL);
 	}
 
 	new_node->n = n;
-
-	for (count = 0; head != NULL && count < idx - 1; count++)
-	{
-		current_node = current_node->next;
-		if (current_node == NULL)
-		{
-			return (NULL);
-		}
-	}
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-	}
-	else if (current_node->next)
-	{
-		new_node->next = current_node->next;
-		current_node->next = new_node;
-	}
-	else
-	{
-		new_node->next = NULL;
-		current_node->next = new_node;
+		return (new_node);
 	}
 
-	return (new_node);
+	for (count = 0; temp_node && count < idx; count++)
+	{
+		if (count == idx - 1)
+		{
+			new_node->next = temp_node->next;
+			temp_node->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			temp_node = temp_node->next;
+		}
+	}
+
+	return (NULL);
 }
