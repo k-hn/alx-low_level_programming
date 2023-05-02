88948e17969e84a482f6c0cf937f106dfa6a1adc
Hilary <125813796+k-hn@users.noreply.github.com>
2023-05-02 16:00:00 +0000

    refactor 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = *head;
 		*head = new_node;
 	}
-	else if(current_node->next)
+	else if (current_node->next)
 	{
 		new_node->next = current_node->next;
 		current_node->next = new_node;
